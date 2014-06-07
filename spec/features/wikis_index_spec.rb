5d823a63531e5770ee7224b34d4b4ca8f145d559
Jonathan Linowes <linojon@gmail.com>
2014-06-06 22:00:11 -0400

    fix rspec

--- spec/features/wikis_index_spec.rb
+++ spec/features/wikis_index_spec.rb
@@ -1,14 +1,18 @@
 require 'spec_helper'
 
 describe "/wikis index" do
-  let!(:user) { create :user }
-  let!(:wikis) { 1.upto(5) { user.wikis << create(:wiki) } }
+  let(:user) { create :user }
+  let(:wikis) { 1.upto(5).map { create(:wiki) } }
+
+  before :each do
+    user.wikis = wikis
+  end
 
   context "as public" do
     it "displays list of the public wikis" do
       visit "/wikis"
-      expect(page).to have_content( "Wiki 1")
-      expect(page).to have_content( "Wiki 5")
+      expect(page).to have_content( wikis.first.title )
+      expect(page).to have_content( wikis.last.title )
     end
     xit "doesnt show private wikis"
   end
