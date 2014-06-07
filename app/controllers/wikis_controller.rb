723ea080b214dd7641333cd7fc180a2e42cf0b3b
Jonathan Linowes <linojon@gmail.com>
2014-06-07 00:05:24 -0400

    edit wiki action

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -13,6 +13,10 @@ class WikisController < ApplicationController
     @wiki = Wiki.new
   end
 
+  def edit
+    @wiki = Wiki.find params[:id]
+  end
+
   def create
     @wiki = current_user.wikis.build(wiki_params)
     if @wiki.save
@@ -20,6 +24,16 @@ class WikisController < ApplicationController
     else
       flash[:error] = "Error creating wiki. Please try again"
       render :new
+    end
+  end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    if @wiki.update_attributes(wiki_params)
+      redirect_to @wiki
+    else
+      flash[:error] = "Error saving wiki. Please try again"
+      render :edit
     end
   end
 
--- spec/features/wiki_new_edit_spec.rb
+++ spec/features/wiki_new_edit_spec.rb
@@ -29,7 +29,22 @@ describe "/wikis new and edit" do
   end
 
   context "edit" do
+    let(:wiki) { create :wiki }
+
+    before :each do
+      visit "/wikis/#{wiki.id}/edit"
+    end
+
     xit "requires user signed in"
-    xit "can edit title, content, and save"
+
+    it "can edit title, content, and save" do
+      expect(page).to have_field('Title', with: 'Wiki 1')
+      expect(page).to have_field('Content', with: 'MyText')
+      fill_in 'Title', with: 'Hello Title'
+      fill_in 'Content', with: 'Hello world.'
+      click_button 'Update Wiki'
+      expect(page).to have_content('Hello Title')
+      expect(page).to have_content('Hello world.')
+    end
   end
  end
